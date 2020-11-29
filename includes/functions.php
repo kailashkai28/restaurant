05170a11c0f5df257e36fb8b64609bbf18f4180e
kailash chandra <37248387+kailashkai28@users.noreply.github.com>
2020-11-29 23:30:29 +0530

    Add files via upload

---
+++ includes/functions.php
@@ -0,0 +1,13 @@
+<?php
+
+function loggedIn(){
+    
+    if(isset($_SESSION['user_email']) || isset($_COOKIE['user_email'])){
+    return true;
+    }
+    else return false;
+    exit();
+}
+
+
+?>
